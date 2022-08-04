355183f7065df175f557806904db87bf1683dc76
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-08-05 00:00:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Write your solution in this file!
+const employee = {
+    name: "Some name",
+    streetAddress: "Some address",
+};
+
+//function 1
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+        ...obj,
+        [key]: value,
+    };
+}
+
+const updatedEmployee = updateEmployeeWithKeyAndValue(
+    employee, "name", "Another One"
+);
+
+employee.name;
+
+//function 2
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+
+    return obj;
+}
+
+const destructivelyUpdatedEmployee = updateEmployeeWithKeyAndValue(
+    employee, "name", "Destructively added"
+);
+
+employee.name;
+
+//function 3
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = {...obj};
+    delete newObj[key];
+
+    return newObj;
+}
+
+const deleteEmployee = deleteFromEmployeeByKey(
+    employee, "name"
+);
+
+employee.name;
+
+//function 4
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+
+    return obj;
+}
+
+const destructivelyDeleteEmployee = deleteFromEmployeeByKey(
+    employee, "name"
+);
+
+employee.name;
